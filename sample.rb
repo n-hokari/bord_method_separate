6b1a3de51ac0e99a07efb6d04c734aed01849d58
n-hokari <hokari1204@gmail.com>
2024-04-28 23:54:50 +0900

    Initial commit

---
+++ sample.rb
@@ -0,0 +1,48 @@
+while true
+  puts "実施したい処理を選択してください"
+  puts "1:評価ポイントとコメントを入力する"
+  puts "2:今までの結果を確認する"
+  puts "3:やめる"
+  num = gets.to_i
+
+  def input_point_comment
+    puts "1から5で評価を入力してください"
+    point = gets.to_i
+    while true
+      if point <= 0 || point > 5
+        puts "1から5で入力してください"
+        point = gets.to_i
+      else
+        puts "コメントを入力してください"
+        comment = gets
+        post = "ポイント：#{point}　コメント：#{comment}"
+        File.open("data.txt", "a") do |file|
+          file.puts(post)
+        end
+        break
+      end
+    end
+  end
+ def  show_result
+    puts "これまでの結果"
+    File.open("data.txt", "r") do |file|
+      file.each_line do |line|
+        puts line
+      end
+    end
+ end  
+  
+  case num
+  when 1
+    # input_point_commentメソッド呼び出し
+    puts input_point_comment
+  when 2
+    # show_resultメソッド呼び出し
+    puts show_result
+  when 3
+    puts "終了します"
+    break
+  else
+    puts "1から3で入力してください"
+  end
+end
